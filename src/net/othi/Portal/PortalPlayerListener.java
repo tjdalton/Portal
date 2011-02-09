e6e89e513df6128d67d7d24d7efa0566f38580d8
othimind <the.othi@gmail.com>
2011-02-10 08:43:13 +1100

    Various mods

--- src/net/othi/Portal/PortalPlayerListener.java
+++ src/net/othi/Portal/PortalPlayerListener.java
@@ -23,6 +23,7 @@ public class PortalPlayerListener extends PlayerListener {
 	public PortalPlayerListener(Portal instance) {
 		plugin = instance;
 	}
+
 	// Portal layout
 	int[][] matrix = { { 49, 49, 49, 49 }, { 49, 0, 0, 49 }, { 49, 0, 0, 49 },
 			{ 49, 0, 0, 49 }, { 49, 49, 49, 49 } };
@@ -33,50 +34,62 @@ public class PortalPlayerListener extends PlayerListener {
 		double factor;
 		// Check which world player is currently in, and set factor
 		if (loc.getWorld().getEnvironment() == World.Environment.NORMAL) {
-			for(int i =0;i<plugin.getServer().getWorlds().size();i++){
-			world = plugin.getServer().getWorlds().get(i);
-			if(world.getEnvironment()==World.Environment.NETHER)
-				break;
+			for (int i = 0; i < plugin.getServer().getWorlds().size(); i++) {
+				world = plugin.getServer().getWorlds().get(i);
+				if (world.getEnvironment() == World.Environment.NETHER)
+					break;
 			}
 			factor = 1/16;
 		} else {
-			for(int i =0;i<plugin.getServer().getWorlds().size();i++){
+			for (int i = 0; i < plugin.getServer().getWorlds().size(); i++) {
 				world = plugin.getServer().getWorlds().get(i);
-				if(world.getEnvironment()==World.Environment.NORMAL)
+				if (world.getEnvironment() == World.Environment.NORMAL)
 					break;
-				}
+			}
 			factor = 16;
 		}
-		//factor = 1;
+		 factor = 1;
 		// Check if player is standing on a Netherportal.
 		if ((loc.getWorld().getBlockTypeIdAt(loc.getBlockX(),
-				loc.getBlockY() - 1, loc.getBlockZ()) == Material.OBSIDIAN.getId())) {
+				loc.getBlockY() - 1, loc.getBlockZ()) == Material.OBSIDIAN
+				.getId())) {
 			if ((loc.getWorld().getBlockTypeIdAt(loc.getBlockX(),
-					loc.getBlockY(), loc.getBlockZ()) == Material.PORTAL.getId())) {
+					loc.getBlockY(), loc.getBlockZ()) == Material.PORTAL
+					.getId())) {
 				Location newLoc = new Location(world, loc.getX() * factor,
 						loc.getY(), loc.getZ() * factor, loc.getPitch(),
 						loc.getYaw());
+				//newLoc.setX(newLoc.getX()-2);
+				//newLoc.setY(newLoc.getY()-2);
+				if(!world.isChunkLoaded(newLoc.getBlockX(), newLoc.getBlockY())){
+					world.loadChunk(newLoc.getBlockX(), newLoc.getBlockY());
+				}
 				// Check to see if we need to make a new connecting portal
 				if (!(((newLoc.getWorld().getBlockTypeIdAt(newLoc.getBlockX(),
-						newLoc.getBlockY() - 1, newLoc.getBlockZ()) == Material.OBSIDIAN.getId()) && (newLoc
-						.getWorld().getBlockTypeIdAt(newLoc.getBlockX() - 1,
-								newLoc.getBlockY() - 1, newLoc.getBlockZ()) == Material.OBSIDIAN.getId())) || ((newLoc
+						newLoc.getBlockY() - 1, newLoc.getBlockZ()) == Material.OBSIDIAN
+						.getId()) && (newLoc.getWorld().getBlockTypeIdAt(
+						newLoc.getBlockX() - 1, newLoc.getBlockY() - 1,
+						newLoc.getBlockZ()) == Material.OBSIDIAN.getId())) || ((newLoc
 						.getWorld().getBlockTypeIdAt(newLoc.getBlockX() + 1,
-								newLoc.getBlockY() - 1, newLoc.getBlockZ()) == Material.OBSIDIAN.getId())
+								newLoc.getBlockY() - 1, newLoc.getBlockZ()) == Material.OBSIDIAN
+						.getId())
 						&& (newLoc.getWorld().getBlockTypeIdAt(
 								newLoc.getBlockX(), newLoc.getBlockY() - 1,
-								newLoc.getBlockZ()) == Material.OBSIDIAN.getId())
+								newLoc.getBlockZ()) == Material.OBSIDIAN
+								.getId())
 						|| ((newLoc.getWorld().getBlockTypeIdAt(
 								newLoc.getBlockX(), newLoc.getBlockY() - 1,
-								newLoc.getBlockZ() + 1) == Material.OBSIDIAN.getId()) && (newLoc
-								.getWorld().getBlockTypeIdAt(
-										newLoc.getBlockX(),
+								newLoc.getBlockZ() + 1) == Material.OBSIDIAN
+								.getId()) && (newLoc.getWorld()
+								.getBlockTypeIdAt(newLoc.getBlockX(),
 										newLoc.getBlockY() - 1,
-										newLoc.getBlockZ()) == Material.OBSIDIAN.getId())) || ((newLoc
-						.getWorld().getBlockTypeIdAt(newLoc.getBlockX(),
-								newLoc.getBlockY() - 1, newLoc.getBlockZ() - 1) == Material.OBSIDIAN.getId()) && (newLoc
-						.getWorld().getBlockTypeIdAt(newLoc.getBlockX(),
-								newLoc.getBlockY() - 1, newLoc.getBlockZ()) == Material.OBSIDIAN.getId()))))) {
+										newLoc.getBlockZ()) == Material.OBSIDIAN
+								.getId())) || ((newLoc.getWorld()
+						.getBlockTypeIdAt(newLoc.getBlockX(),
+								newLoc.getBlockY() - 1, newLoc.getBlockZ() - 1) == Material.OBSIDIAN
+						.getId()) && (newLoc.getWorld().getBlockTypeIdAt(
+						newLoc.getBlockX(), newLoc.getBlockY() - 1,
+						newLoc.getBlockZ()) == Material.OBSIDIAN.getId()))))) {
 					System.out.println("Recycling Portal");
 					int x = 0;
 					// Try to avoid creating a portal in the air, move down if
@@ -86,10 +99,12 @@ public class PortalPlayerListener extends PlayerListener {
 							newLoc.getBlockZ()) == Material.AIR.getId()
 							|| newLoc.getWorld().getBlockTypeIdAt(
 									newLoc.getBlockX(), newLoc.getBlockY() - 1,
-									newLoc.getBlockZ()) == Material.OBSIDIAN.getId()
+									newLoc.getBlockZ()) == Material.OBSIDIAN
+									.getId()
 							|| newLoc.getWorld().getBlockTypeIdAt(
 									newLoc.getBlockX(), newLoc.getBlockY() - 1,
-									newLoc.getBlockZ()) == Material.PORTAL.getId()) {
+									newLoc.getBlockZ()) == Material.PORTAL
+									.getId()) {
 						if (newLoc.getBlockY() >= 2) {
 							newLoc.setY(newLoc.getY() - 1);
 							x--;
@@ -100,11 +115,17 @@ public class PortalPlayerListener extends PlayerListener {
 					// Try to avoid creating a portal in solid rock, move up if
 					// so
 					while (newLoc.getWorld().getBlockTypeIdAt(
-							newLoc.getBlockX() + 1, newLoc.getBlockY() + 1,
+							newLoc.getBlockX() + 1, newLoc.getBlockY(),
 							newLoc.getBlockZ()) != Material.AIR.getId()
 							&& newLoc.getWorld().getBlockTypeIdAt(
-									newLoc.getBlockX() + 2,
-									newLoc.getBlockY() + 1, newLoc.getBlockZ()) != Material.AIR.getId()) {
+									newLoc.getBlockX() + 2, newLoc.getBlockY(),
+									newLoc.getBlockZ()) != Material.AIR.getId()
+							&& newLoc.getWorld().getBlockTypeIdAt(
+									newLoc.getBlockX() + 3, newLoc.getBlockY(),
+									newLoc.getBlockZ()) != Material.AIR.getId()
+							&& newLoc.getWorld().getBlockTypeIdAt(
+									newLoc.getBlockX() + 4, newLoc.getBlockY(),
+									newLoc.getBlockZ()) != Material.AIR.getId()) {
 						if (newLoc.getBlockY() <= 119) {
 							newLoc.setY(newLoc.getY() + 1);
 							x++;
@@ -114,11 +135,19 @@ public class PortalPlayerListener extends PlayerListener {
 					System.out.println("Adjusting Y Up: " + x);
 					// Make sure there is enough room to spawn
 					for (int i = 0; i < 5; i++) {
-						for (int j = 1; j < 2; j++) {
+						for (int j = 1; j < 3; j++) {
 							// for (int k = -2; k < 2; k++) {
-							world.getBlockAt(newLoc.getBlockX() + i,
-									newLoc.getBlockY() + j, newLoc.getBlockZ())
+							world.getBlockAt(newLoc.getBlockX() + j,
+									newLoc.getBlockY() + i, newLoc.getBlockZ())
 									.setTypeId(Material.AIR.getId());
+							world.getBlockAt(newLoc.getBlockX() + j,
+									newLoc.getBlockY() + i,
+									newLoc.getBlockZ() - 1).setTypeId(
+									Material.AIR.getId());
+							world.getBlockAt(newLoc.getBlockX() + j,
+									newLoc.getBlockY() + i,
+									newLoc.getBlockZ() + 1).setTypeId(
+									Material.AIR.getId());
 							// }
 						}
 					}
@@ -130,15 +159,17 @@ public class PortalPlayerListener extends PlayerListener {
 								if (newLoc.getWorld().getBlockTypeIdAt(
 										newLoc.getBlockX() + j,
 										newLoc.getBlockY() + i,
-										newLoc.getBlockZ() + k) == Material.OBSIDIAN.getId()
+										newLoc.getBlockZ() + k) == Material.OBSIDIAN
+										.getId()
 										|| newLoc.getWorld().getBlockTypeIdAt(
 												newLoc.getBlockX() + j,
 												newLoc.getBlockY() + i,
-												newLoc.getBlockZ() + k) == Material.PORTAL.getId()) {
+												newLoc.getBlockZ() + k) == Material.PORTAL
+												.getId()) {
 									world.getBlockAt(newLoc.getBlockX() + j,
 											newLoc.getBlockY() + i,
-											newLoc.getBlockZ() + k)
-											.setTypeId(Material.AIR.getId());
+											newLoc.getBlockZ() + k).setTypeId(
+											Material.AIR.getId());
 								}
 							}
 						}
@@ -158,11 +189,12 @@ public class PortalPlayerListener extends PlayerListener {
 								if (newLoc.getWorld().getBlockTypeIdAt(
 										newLoc.getBlockX() + j,
 										newLoc.getBlockY() + i,
-										newLoc.getBlockZ() + k) == Material.PORTAL.getId()) {
+										newLoc.getBlockZ() + k) == Material.PORTAL
+										.getId()) {
 									world.getBlockAt(newLoc.getBlockX() + j,
 											newLoc.getBlockY() + i,
-											newLoc.getBlockZ() + k)
-											.setTypeId(Material.AIR.getId());
+											newLoc.getBlockZ() + k).setTypeId(
+											Material.AIR.getId());
 								}
 							}
 						}
@@ -175,7 +207,8 @@ public class PortalPlayerListener extends PlayerListener {
 				// Update from/to to avoid 'player moved wrongly'
 				event.setFrom(newLoc);
 				event.setTo(newLoc);
-				//System.out.println("Worlds: " + newLoc.getWorld().getEnvironment().toString());
+				// System.out.println("Worlds: " +
+				// newLoc.getWorld().getEnvironment().toString());
 				event.getPlayer().teleportTo(newLoc);
 				System.out.println(event.getPlayer().getDisplayName()
 						+ " was transported to: "
